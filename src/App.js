ac626008a1358c5f5e8ce7d16a0f640cc9c21d67
Jiannkim <jiahnkimm@gmail.com>
2024-11-09 23:29:19 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import wiskydata from './data/menuData.json';
 import './App.css';
 
 function App() {
+  const data = wiskydata;
+
+  const [selectedCategory, setSelectedCategory] = useState(data[0].category);
+  const [items, setItems] = useState(data[0].items);
+
+  const handleCategoryClick = (category) => {
+    const selected = data.find(item => item.category === category);
+    setSelectedCategory(selected.category);
+    setItems(selected.items);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header header-font">
+        HANOOK's Menu
       </header>
+      <CategoryNav categories={data} onCategoryClick={handleCategoryClick} />
+      <div className='menu-container'>
+        <MenuItems items={items} />
+      </div>
     </div>
   );
 }
 
+function CategoryNav({ categories, onCategoryClick }) {
+  return (
+    <div className="category-nav">
+      <ul>
+      {categories.map(category => (
+        <li key={category.category} onClick={() => onCategoryClick(category.category)}>
+        {category.category}</li>
+      ))}
+      </ul>
+    </div>
+  );
+}
+
+function MenuItems({ items }) {
+  return (
+    <div className="menu-items">
+      {items.map(item => (
+        <div className="menu-item" key={item.name}>
+          <img src={item.image} alt={item.name} />
+          <h3>{item.name}</h3>
+          <p>{item.price}</p>
+          <p>{item.description}</p>
+          <p>{item.description_en}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
 export default App;
